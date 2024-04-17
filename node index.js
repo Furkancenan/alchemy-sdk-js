8ca1b6ac8ffb762e8155baf389756bd435e90132
Furkancenan <136871633+Furkancenan@users.noreply.github.com>
2024-04-17 04:04:13 +0300

    Create  node index.js

---
+++ node index.js
@@ -0,0 +1,13 @@
+npm install alchemy-sdk
+import { Network, Alchemy } from 'alchemy-sdk';
+
+const settings = {
+  apiKey: "Ax8DiiNUKIk42r9Sx1BMG5wQdz7OaBJL",
+  network: Network.ARB_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+// get the latest block
+const latestBlock = alchemy.core.getBlock("latest").then(console.log)
+node index.js
